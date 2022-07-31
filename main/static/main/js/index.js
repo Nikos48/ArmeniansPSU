0a5f2566c53fb54c80c257eca05d14076c814ab3
Nikos48 <episkoposyan2004@mail.ru>
2022-07-31 16:29:54 +0300

    stuff

--- main/static/main/js/index.js
+++ main/static/main/js/index.js
@@ -1,1 +1,20 @@
+const hamburger = document.querySelector(".hamburger");
+const navUl = document.querySelector(".nav-ul");
+if (hamburger) {
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navUl.classList.toggle("active");
+})}
 
+
+const phoneNumber = "+79624453363"
+function copyPhone() { 
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText("+79624453363")
+    .then(() => {
+      alert("Номер Скопирован");
+    })
+    .catch(() => {
+      alert("Что-то пошло не так");
+    })
+}
