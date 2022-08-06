d8ccd8b9574efc8e793897885e6c9c341f060b37
Nikos48 <episkoposyan2004@mail.ru>
2022-08-06 17:45:42 +0300

    debug = true

--- ArmeniansPSU/settings.py
+++ ArmeniansPSU/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l*xsxhu&9!@h1ien!-nnch+e!zrs#%n#*9+=jm969z6+g+qljl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://armenians-psu.herokuapp.com']
 
