4b50812aa981a716b4a9b99adab11574c01e7837
Nikos48 <episkoposyan2004@mail.ru>
2022-08-06 17:06:12 +0300

    added host

--- ArmeniansPSU/settings.py
+++ ArmeniansPSU/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-l*xsxhu&9!@h1ien!-nnch+e!zrs#%n#*9+=jm969z6+g+qljl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://armenians-psu.herokuapp.com']
+ALLOWED_HOSTS = ['https://armenians-psu.herokuapp.com', '*']
 
 
 # Application definition
